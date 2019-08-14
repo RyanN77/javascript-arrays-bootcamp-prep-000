680e302fd0986692e8ee964adfb39d4358f8e058
adventurous-class-0665 <rngao97@gmail.com>
2019-08-14 20:31:21 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -12,3 +12,21 @@ function destructivelyAddElementToBeginningOfArray(array, element){
   array.unshift(element)
   return array
 }
+
+function addElementToEndOfArray(array, element){
+  return [...array, element]
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element)
+  return array
+}
+
+function accessElementInArray(array, index){
+  return array[index]
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+  array.shift()
+  return array
+}
