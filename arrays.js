5ab01bcc52fc6c71c84f8df2880c09e2bc353456
adventurous-class-0665 <rngao97@gmail.com>
2019-08-14 20:35:19 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -35,3 +35,13 @@ function removeElementFromBeginningOfArray(array){
   array.slice(1)
   return array
 }
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  array.pop()
+  return array
+}
+
+function removeElementFromEndOfArray(array){
+  array.slice(0, array.length - 1)
+  return array
+}
